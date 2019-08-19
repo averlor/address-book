c6878261149be0a5860c15629fab02a197e9253a
Vladimir Abramyants <abramyants@aurum.org>
2019-08-19 09:57:19 +0300

    Deleted other files

--- src/version_2/Person.py
+++
@@ -1,9 +0,0 @@
-class Person:
-
-    def __init__(self, id, name, familyname, lastname, address, phone):
-        self.id = id
-        self.name = name
-        self.familyname = familyname
-        self.lastname = lastname
-        self.address = address
-        self.phone = phone
