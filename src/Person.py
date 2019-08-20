a389ff737e20981713bb77cafe05c78a86b062b3
Vladimir A <avabramanc@gmail.com>
2019-08-20 23:56:38 +0300

    Update constuctor

--- src/Person.py
+++ src/Person.py
@@ -1,9 +1,12 @@
 class Person:
 
-    def __init__(self, id, name, familyname, lastname, address, phone):
+    def __init__(self, id, name, familyname, lastname, address, phone,
+                 date_create, date_modify):
         self.id = id
         self.name = name
         self.familyname = familyname
         self.lastname = lastname
         self.address = address
         self.phone = phone
+        self.datecreate = date_create
+        self.datemodify = date_modify
